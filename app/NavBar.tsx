d2392057c225f2fa18c9116080a44e477500d440
janesfer <sferrazzajane@gmail.com>
2024-02-26 13:18:17 -0500

    dashboard started

---
+++ app/NavBar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { icons } from "react-icons";
+
+const NavBar = () => {
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        <li>
+          <Link href="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/issues">Issues</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
