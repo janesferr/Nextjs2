53eb4abd7ae52ba6de7fe1b1e73eab30dabe8d27
janesfer <sferrazzajane@gmail.com>
2024-02-27 16:04:55 -0500

    New radix ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { Button } from "@radix-ui/themes";
 
-export default function Home() {
-  return <div>Hello World</div>;
+export default function IssuePage() {
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 }
